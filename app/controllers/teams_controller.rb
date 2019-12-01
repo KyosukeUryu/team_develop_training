18f2c163ad82c252ec9074a15c15b8392a611b3b
kyosuke uryu <kyo.uryu33@gmail.com>
2019-12-01 15:27:09 +0900

    [add]オーナー移譲のためのルーティング設定

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -48,6 +48,9 @@ class TeamsController < ApplicationController
     @team = current_user.keep_team_id ? Team.find(current_user.keep_team_id) : current_user.teams.first
   end
 
+  def change_owner
+  end
+
   private
 
   def set_team
